827f8b0b5c5baae2fe8451bc45bd090baf368e0b
Kimo Adel <kimodev1990@gmail.com>
2024-03-26 21:40:01 +0300

    files upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        paleBrown: "#FEFFEE",
+        colorTitle: "#EF8B08",
+        borderColor: "#CC9966",
+        borderLineColor: "#870040",
+        textColor: "#800040",
+        bgColor: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
